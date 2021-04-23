ff9b34daf7b31800ada734a8bba52978a8343bd1
Javier5Albatros <javieralvarez2.ja@gmail.com>
2021-04-23 16:53:30 +0200

    Ad

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,8 +2,16 @@ const User = require("../models/User");
 const Log = require("../models/Log");
 
 const getLogs = async (req, res) => {
-  const user = req.user;
-  await User.populate(user, "log");
+  const user = new User(req.user);
+  const { from, to, limit } = req.query;
+
+  await User.populate(user, {
+    path: "log",
+    perDocumentLimit: limit || 0,
+    match: {
+      date: [{ $gte: from || 0 }, { $lte: to || 0 }],
+    },
+  });
   const log = new Log({ user, count: user.log.length });
   return res.json({
     _id: user.id,
