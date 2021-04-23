6a5765a781e3e042c05f42bc200576b74960d818
Javier5Albatros <javieralvarez2.ja@gmail.com>
2021-04-23 16:39:02 +0200

    NOW?

--- controllers/logs.js
+++ controllers/logs.js
@@ -5,7 +5,12 @@ const getLogs = async (req, res) => {
   const user = req.user;
   await User.populate(user, "log");
   const log = new Log({ user, count: user.log.length });
-  return res.json(log);
+  return res.json({
+    _id: user.id,
+    username: user.username,
+    count: log.count,
+    log: user.exercises,
+  });
 };
 
 module.exports = { getLogs };
