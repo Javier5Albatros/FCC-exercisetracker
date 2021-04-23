1e36ae415b8a3f2e5608121990bd06806632bc7b
Javier5Albatros <javieralvarez2.ja@gmail.com>
2021-04-23 16:15:39 +0200

    asd

--- models/User.js
+++ models/User.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    log: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exercise" }],
   },
-  log: [{ type: mongoose.Schema.Types.ObjectId, ref: "Exercise" }],
-});
+  { versionKey: false }
+);
 
 const User = mongoose.model("User", userSchema);
 
